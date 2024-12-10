3736e13fb114530acf649a4fa41607e443d7f2c7
Bettroll <gilbertawak47@gmail.com>
2024-12-10 15:13:33 +0700

    perbaikan final

--- random_card.cpp
+++ random_card.cpp
@@ -7,12 +7,10 @@
 using namespace std;
 
 int main() {
-    // Inisialisasi deck kartu
     string suits[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
     string ranks[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     string deck[52];
 
-    // Membuat deck kartu
     int index = 0;
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 13; ++j) {
@@ -20,25 +18,21 @@ int main() {
         }
     }
 
-    // Mengacak deck secara manual
-    srand(time(0)); // Seed untuk random
+    srand(time(0));
     for (int i = 0; i < 52; ++i) {
         int randomIndex = rand() % 52;
-        // Tukar posisi deck[i] dengan deck[randomIndex]
         string temp = deck[i];
         deck[i] = deck[randomIndex];
         deck[randomIndex] = temp;
+        cout << deck[randomIndex] << endl;
     }
 
-    // Memilih kartu acak untuk ditebak
     string selectedCard = deck[rand() % 52];
 
-    // Fitur menebak kartu
     cout << "Tebak kartu (format: 'Rank of Suit', contoh: 'Ace of Spades'): ";
     string userGuess;
     getline(cin, userGuess);
 
-    // Cek apakah tebakan benar
     if (userGuess == selectedCard) {
         cout << "Tebakanmu benar! Kartunya adalah " << selectedCard << "." << endl;
     } else {
